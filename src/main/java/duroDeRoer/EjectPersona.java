6711413adac30e891da7732075cdef9a26d4bab6
LuisMiguel <LuisMiguel@DESKTOP-KRN8NS5>
2021-08-13 17:50:40 +0200

    Hasta la información total de los objetos.

--- src/main/java/duroDeRoer/EjectPersona.java
+++ src/main/java/duroDeRoer/EjectPersona.java
@@ -1,6 +1,7 @@
 package duroDeRoer;
 
 import javax.swing.JOptionPane;
+import java.lang.*;
 
 /**
  *
@@ -12,12 +13,16 @@ public class EjectPersona {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        Persona persona1=new Persona();
-        String suNombre, suSexo, suEdad, suPeso, suAltura;
-        double pesoNum, alturaNum;
-        int edadNum;
         
+        Persona persona1=new Persona();
+        Persona persona2=new Persona();
+        Persona persona3=new Persona();
+        
+        String suNombre, suEdad, suSexo, suPeso, suAltura;
+        double pesoNum, alturaNum;
+        char sex;
+        int edadNum;
+            
         suNombre=JOptionPane.showInputDialog("Introduzca su nombre: ");
         edadNum=Integer.parseInt(JOptionPane.showInputDialog("Introduzca su edad: "));
         suSexo=JOptionPane.showInputDialog("Introduzca su sexo: ");
@@ -26,9 +31,28 @@ public class EjectPersona {
         
         persona1.setNombre(suNombre);
         persona1.setEdad(edadNum);
-        persona1.setSexo(suSexo);
+        //persona1.setSexo(suSexo);
         persona1.setPeso(pesoNum);
         persona1.setAltura(alturaNum);
+        
+        System.out.println("La primera persona, que se llama "+ suNombre +
+                ", con DNI nº:" + persona1.generarDNI() + persona1.esMayorDeEdad(edadNum) + "\n");
+        if (persona1.calcularIMC(alturaNum, pesoNum)==0){
+            System.out.println(suNombre + " está en su peso ideal.");
+        } else if (persona1.calcularIMC(alturaNum, pesoNum)==-1){
+            System.out.println(suNombre + " está por debajo de su peso ideal.");
+        } else {
+            System.out.println(suNombre + " está por encima de su peso ideal.");
+        }
+        
+        //suNombre=JOptionPane.showInputDialog("Introduzca su nombre: ");
+        //edadNum=Integer.parseInt(JOptionPane.showInputDialog("Introduzca su edad: "));
+        //suSexo=JOptionPane.showInputDialog("Introduzca su sexo: ");
+        
+        //persona2.getNombre(suNombre);
+        //persona2.setEdad(edadNum);
+        //persona2.setSexo(sex);
+        
     }
     
 }
