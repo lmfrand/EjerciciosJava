005c0ed21e92c0a98021f2c96e31772651c3189b
LuisMiguel <LuisMiguel@DESKTOP-KRN8NS5>
2021-08-15 20:44:21 +0200

    Añadido método precioFinal.

--- src/main/java/Ej3_Electrodomesticos/Electrodomestico.java
+++ src/main/java/Ej3_Electrodomesticos/Electrodomestico.java
@@ -34,16 +34,13 @@ public class Electrodomestico {
     public int getPrecio_base() {
         return precio_base;
     }
-
     public int getPeso() {
         return peso;
     }
-    
-    public String getColor() {
+        public String getColor() {
         return color;
     }
-    
-    public char getConsumo_energetico() {
+        public char getConsumo_energetico() {
         return consumo_energetico;
     }
     
@@ -51,17 +48,50 @@ public class Electrodomestico {
     public void setPrecio_base(int precio_base) {
         this.precio_base = precio_base;
     }
-  
-    public void setPeso(int peso) {
+      public void setPeso(int peso) {
         this.peso = peso;
     }
-   
-    public void setColor(String color) {
+       public void setColor(String color) {
         this.color = color;
     }
-  
-    public void setConsumo_energetico(char consumo_energetico) {
+      public void setConsumo_energetico(char consumo_energetico) {
         this.consumo_energetico = consumo_energetico;
     } 
     
+    public int precioFinal(){
+        
+        int precio_final, precioEne;
+        
+        switch (consumo_energetico){
+            case 'A':
+                precioEne=precio_base+100;
+                break;
+            case 'B':
+                precioEne=precio_base+80;
+                break;
+            case 'C':
+                precioEne=precio_base+60;
+                break;
+            case 'D':
+                precioEne=precio_base+50;
+                break;
+            case 'E':
+                precioEne=precio_base+30;
+                break;
+            default:
+                precioEne=precio_base+10;                 
+        }
+                
+        if (peso>0 && peso<20){
+            precio_final=precioEne+10;
+        } else if (peso>19 && peso<50){
+            precio_final=precioEne+50;
+        } else if (peso>49 && peso<80){
+            precio_final=precioEne+80;
+        } else {
+            precio_final=precioEne+100;
+        }
+        
+        return precio_final;
+    }
 }
