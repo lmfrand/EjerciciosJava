3f957ed1ecc762191bc870b0b4a9d5d7235683f2
LuisMiguel <LuisMiguel@DESKTOP-KRN8NS5>
2021-08-15 20:56:52 +0200

    Prueba del método precioFinal().

--- src/main/java/Ej3_Electrodomesticos/GoElectrodomestico.java
+++ src/main/java/Ej3_Electrodomesticos/GoElectrodomestico.java
@@ -4,6 +4,10 @@ public class GoElectrodomestico {
 
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        Lavadora e=new Lavadora(54, 20);
+        System.out.println("El precio final de la lavadora es de " +
+                e.precioFinal() + "€");
     }
     
 }
