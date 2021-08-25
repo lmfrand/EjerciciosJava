f8dd488bc23b644cf7f986704d94a4bd8cda8802
LuisMiguel <LuisMiguel@DESKTOP-KRN8NS5>
2021-08-25 18:01:51 +0200

    Clase Lavadora terminada y comprobada

--- src/main/java/Ej3_Electrodomesticos/GoElectrodomestico.java
+++ src/main/java/Ej3_Electrodomesticos/GoElectrodomestico.java
@@ -5,9 +5,10 @@ public class GoElectrodomestico {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        Lavadora e=new Lavadora(54, 20);
+        Lavadora e=new Lavadora(100, 20, 'C', "Blanco", 34);
         System.out.println("El precio final de la lavadora es de " +
-                e.precioFinal() + "€");
+                e.precioLavadora(e)+ "€");
+                //System.out.println(e.getPeso());
     }
     
 }
--- src/main/java/Ej3_Electrodomesticos/Lavadora.java
+++ src/main/java/Ej3_Electrodomesticos/Lavadora.java
@@ -3,6 +3,7 @@ package Ej3_Electrodomesticos;
 public class Lavadora extends Electrodomestico {
     private int carga;
     private final static int CARGA_DEF=5;
+    private int precioFin;
     
     public Lavadora(){
         this(PRECIO_DEF, PESO_DEF, CONENE_DEF, COLOR_DEF, CARGA_DEF);
@@ -16,5 +17,20 @@ public class Lavadora extends Electrodomestico {
     public Lavadora (int precio_base, int peso, char consumo_energetico, String color, int carga){
        super(precio_base, peso, consumo_energetico, color);
        this.carga=carga;
+    }
+    
+    public int getCarga(){
+        return carga;
+        
+    } 
+    
+    public int precioLavadora(Electrodomestico e){
+        
+        if (carga<30){
+            precioFin=e.precioFinal();
+        } else {
+            precioFin=e.precioFinal()+50;
+        }
+        return precioFin;
     }
 }
